0006c998457d0a84f05952e73be0902ad4b391e2
Eyüp <epsilonelittr@gmail.com>
2025-04-03 10:56:42 +0300

    Create a simple foblex flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import {
+  EFResizeHandleType,
+  FCanvasComponent,
+  FFlowModule,
+} from '@foblex/flow';
+import { PointExtensions } from '@foblex/2d';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FFlowModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'foblex-flow-elkjs-example';
+  @ViewChild(FCanvasComponent, { static: true })
+  public fCanvas!: FCanvasComponent;
+
+  protected readonly eResizeHandleType = EFResizeHandleType;
+
+  public onLoaded(): void {
+    this.fCanvas.fitToScreen(PointExtensions.initialize(50, 50), false);
+  }
 }
